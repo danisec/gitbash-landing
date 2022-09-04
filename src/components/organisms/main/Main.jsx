b749a96a7fc96d6e8f5382b64664566780ffbaa3
danisec <daniaprilyanto35@gmail.com>
2022-09-04 17:53:19 +0700

    add library react-github-calendar

--- src/components/organisms/main/Main.jsx
+++ src/components/organisms/main/Main.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+
+import GitHubCalendar from 'react-github-calendar';
 import Tab from '../../molecules/tab/Tab'
 import Card from '../card/Card'
 
 function Main() {
+    const selectLastHalfYear = contributions => {
+        const currentYear = new Date().getFullYear();
+        const currentMonth = new Date().getMonth();
+        const shownMonths = 7;
+
+        return contributions.filter(day => {
+            const date = new Date(day.date);
+            const monthOfDay = date.getMonth();
+
+            return (
+                date.getFullYear() === currentYear &&
+                monthOfDay > currentMonth - shownMonths &&
+                monthOfDay <= currentMonth
+            );
+        });
+    };
     return (
         <>
             <div className='main layout mt-12 mb-12'>
@@ -21,6 +39,19 @@ function Main() {
                             <Card />
                         </div>
 
+                    </div>
+
+                    <div className='mt-20 flex justify-center sm:hidden'>
+                        <GitHubCalendar
+                            username="danisec"
+                            transformData={selectLastHalfYear}
+                        />
+                    </div>
+
+                    <div className='hidden sm:mt-28 sm:flex sm:justify-center'>
+                        <GitHubCalendar
+                            username="danisec"
+                        />
                     </div>
 
                 </div>
